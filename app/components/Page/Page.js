7ad1e81ba35df1ddc2ae1cf8e269e5a66cd8380f
Vasiliy Poverennov <prn.v_poverennov@wargaming.net>
2017-01-24 13:23:08 +0300

    add code

---
+++ app/components/Page/Page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Button } from 'components'
+
+
+class Page extends React.Component {
+    render() {
+        return (
+            <Button>Hello</Button>
+        )
+    }
+}
+
+export default Page
